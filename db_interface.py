493c7be3a8259f3abd335d1cf178df8c52d8be39
good-riverdeer <rornf6232@gmail.com>
2020-11-07 18:57:54 +0900

    "sqlite3 db test" update

---
+++ db_interface.py
@@ -0,0 +1,25 @@
+import sqlite3
+from contextlib import closing
+
+class InterfDB:
+
+    def __init__(self, dbname):
+        self.__DBNAME = dbname
+        self.db = sqlite3.connect(self.__DBNAME)
+
+    # def connect_db(self):
+    #     return sqlite3.connect(self.__DBNAME)
+
+
+    # db = connect_db()
+
+    def init_db(self):
+        with open("schema.sql", 'r') as f:
+            self.db.cursor().executescript(f.read())
+        self.db.commit()
+
+    def query_db(self, query, args=(), one=False):
+        cursor = self.db.execute(query, args)
+        result = [dict((cursor.description[idx][0], value) for idx, value in enumerate(row)) \
+                  for row in cursor.fetchall()]
+        return (result[0] if result else None) if one else result
---
+++ schema.sql
@@ -0,0 +1,5 @@
+create table if not exists user(
+    user_id integer primary key autoincrement,
+    username string not null,
+    score string not null
+);
